0bf8db19cc32279ee398df33a62b92287c3e3b13
adrianpraja <adrian@databank.ro>
2019-10-24 18:42:50 +0300

    update

--- src/stack/parameters.js
+++ src/stack/parameters.js
@@ -6,7 +6,26 @@ export default Ractive.extend({
 		tabledata: tabledata,
 	},
 	template: `
-		<tabledata columns='{{columns}}' rows='{{rows}}' style='top: 10px;' />
+		<table style="border-collapse: collapse;border-spacing: 0; empty-cells: show; border: 1px solid #eaeded;width: 100%;">
+		    <thead style="background-color: #fafafa;color: #000;text-align: left;vertical-align: bottom;border-bottom: 1px solid #eaeded">
+		        <tr>
+		            <th style="padding: 0.5em 1em;">Key</th>
+		            <th style="padding: 0.5em 1em;">Value</th>
+		            <th style="padding: 0.5em 1em;">Resolved value</th>
+		        </tr>
+		    </thead>
+		    <tbody>
+				{{#parameters}}
+		        <tr>
+		            <td style="padding: 0.5em 1em;border-width: 0 0 1px 0;border-bottom: 1px solid #eaeded;">{{.ParameterKey}}</td>
+		            <td style="padding: 0.5em 1em;border-width: 0 0 1px 0;border-bottom: 1px solid #eaeded;">{{.ParameterValue}}</td>
+		            <td style="padding: 0.5em 1em;border-width: 0 0 1px 0;border-bottom: 1px solid #eaeded;"></td>
+		        </tr>
+				{{/parameters}}
+
+		    </tbody>
+		</table>
+
 	`,
 	oninit: function() {
 		var ractive=this;
@@ -26,6 +45,8 @@ export default Ractive.extend({
 
 
 			ractive.set('stack',data.Stacks[0])
+
+			ractive.set('parameters', data.Stacks[0].Parameters )
 
 			ractive.set('rows',
 				data.Stacks[0].Parameters.map(function(p) {
