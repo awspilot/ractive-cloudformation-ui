10a4c4cc05142241382f486f0970736741008877
adrianpraja <adrian@databank.ro>
2019-10-24 20:25:10 +0300

    update

--- src/stack/resources.js
+++ src/stack/resources.js
@@ -6,8 +6,14 @@ export default Ractive.extend({
 	},
 	template: `
 
-		<div style="padding: 7px;text-align: right;background-color: #fafafa;border-bottom: 1px solid #eaeded;">
-			<a class="btn btn-sm btn-default" on-click="@this.list_resources()"><i class="icon zmdi zmdi-refresh"></i></a>
+
+
+		<div style="background-color: #fafafa;border-bottom: 1px solid #eaeded;height: 50px;padding: 7px;">
+			<div style="float:right;">
+				<a class="btn btn-sm btn-default" on-click="@this.list_resources()"><i class="icon zmdi zmdi-refresh"></i></a>
+			</div>
+			<div style="font-size: 20px;line-height: 40px;">Resources</div>
+
 		</div>
 
 		<table style="border-collapse: collapse;border-spacing: 0; empty-cells: show;width: 100%;">
